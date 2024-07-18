52b6c574a4dab49de83b8e72bc8e2ce6084523b3
NamK666 <chriscole77@naver.com>
2024-07-18 15:14:14 +0900

    update

--- src/main/java/com/example/springselfpractice/PostController.java
+++ src/main/java/com/example/springselfpractice/PostController.java
@@ -2,10 +2,7 @@ package com.example.springselfpractice;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -36,6 +33,43 @@ public class PostController {
         post.setCreatedAt(LocalDateTime.now());
         posts.add(post);
         return "redirect:/posts";
+    }
+
+    @GetMapping("/{id}")
+    public String detail(@PathVariable Long id, Model model) {
+        Post post = posts.stream()
+                .filter(p -> p.getId() == id)
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("해당 게시글이 존재하지 않습니다. id: " + id));
+        model.addAttribute("post", post);
+        return "post/detail";
+    }
+
+    @PostMapping("/{id}/delete")
+    public String delete(@PathVariable("id") Long id) {
+        posts.removeIf(p -> p.getId() == id);
+        return "redirect:/posts";
+    }
+
+    @GetMapping("{id}/edit") // 수정 페이지
+    public String editForm(@PathVariable Long id, Model model) {
+        Post post = posts.stream()
+                .filter(p -> p.getId() == id)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 존재하지 않습니다. id: " + id));
+        model.addAttribute("post", post);
+        return "post/edit";
+    }
+
+    @PostMapping("{id}/edit")
+    public String edit(@PathVariable("id") Long id, @ModelAttribute Post updatedPost) {
+        Post post = posts.stream()
+                .filter(p->p.getId()==id)
+                .findFirst()
+                .orElseThrow(()->new IllegalArgumentException("해당 게시글이 존재하지 않습니다. id: " + id));
+        post.setTitle(updatedPost.getTitle());
+        post.setContent(updatedPost.getContent());
+        return "redirect:/posts/{id}";
     }
 
 }
