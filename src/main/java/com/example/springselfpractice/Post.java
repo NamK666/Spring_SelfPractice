c78bb744bfff626db5ec5db440cd19226b02e0c4
NamK666 <chriscole77@naver.com>
2024-07-18 13:41:09 +0900

    update

---
+++ src/main/java/com/example/springselfpractice/Post.java
@@ -0,0 +1,19 @@
+package com.example.springselfpractice;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post {
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+}
--- src/main/java/com/example/springselfpractice/ThymeleafController.java
+++ src/main/java/com/example/springselfpractice/ThymeleafController.java
@@ -1,20 +1,41 @@
-    package com.example.springselfpractice;
+package com.example.springselfpractice;
 
-    import org.springframework.stereotype.Controller;
-    import org.springframework.ui.Model;
-    import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
-    @Controller
-    public class ThymeleafController {
+import java.util.ArrayList;
+import java.util.List;
 
-        @GetMapping("/index123")
-        public String index(Model model) {
-            model.addAttribute("name1", "어트리뷰트벨류값");
-            model.addAttribute("name2","또 연습해보자");
+@Controller
+public class ThymeleafController {
 
-            int[] iterData = {1,2,3,4,5};
-            model.addAttribute("iterData", iterData);
+    @GetMapping("/index123")
+    public String index(Model model) {
+        model.addAttribute("name1", "어트리뷰트벨류값");
+        model.addAttribute("name2", "또 연습해보자");
 
-            return "index";
-        }
+        int[] iterData = {1, 2, 3, 4, 5};
+        model.addAttribute("iterData", iterData);
+
+        return "index";
     }
+
+    @GetMapping("/index/2")
+    public String index2(Model model) {
+        return "index2";
+    }
+
+    @PostMapping("/users")
+    public String users(@ModelAttribute User user) {
+        System.out.println("유저의 이름은 " + user.getUsername() + " 입니다.");
+        System.out.println("유저의 이메일은" + user.getEmail() + " 입니다.");
+        return "index2";
+    }
+
+    private List<Post> posts = new ArrayList<>();
+    private Long nextId = 1L;
+    
+}
